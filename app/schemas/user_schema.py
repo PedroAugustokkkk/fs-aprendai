ac2c0f25d228d7a8557cc991186ad9d705b907d4
Pedro Augusto Santos Magalhães <contatodepedro@gmail.com>
2025-11-02 10:22:13 -0300

    Update user_schema.py

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,18 +1,20 @@
-from app.extensions import ma
-from app.models.user_model import User
-from marshmallow import fields
+from app.extensions import ma 
+from app.models.user_model import User 
+from marshmallow import fields 
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
-    password = fields.String(load_only=True, required=True) 
+    password = fields.String(load_only=True, required=False, allow_none=True) 
 
     class Meta:
         model = User
-        load_instance = False
+        load_instance = False 
         exclude = ("password_hash", "tasks", "chat_histories")
 
     id = ma.auto_field(dump_only=True)
     created_at = ma.auto_field(dump_only=True)
-    email = fields.Email(required=True)
+
+    email = fields.Email(required=False, allow_none=True)
+
     username = fields.String(required=True)
 
 user_schema = UserSchema()
